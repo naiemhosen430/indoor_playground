47391631f13aab66bac0ebd2f896279a5f1223df
naiemhosen430@ <NaiemGIt43@nAiEm>
2024-01-11 08:26:06 -0800

    d

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,184 @@
+ @extends('layout.frontend')
+
+ @section('content')
+
+
+ <!-- --------------------------------- hero start here-------------------------------  -->
+ <section>
+    
+    <!-- slider  -->
+    <div class="custom-slider-item">
+        <img src="https://html.rometheme.pro/kids/images/homeslide2.jpg" alt="">
+
+        <div class="slider-content">
+            <div>
+                <h1 class="fw-bolder text-light py-5">Best Kindergerden at Califonia</h1>
+                <a href="/">
+                    Learn More
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- hero bottom box  -->
+    <div class="hero-bottom-box text-light text-center row justify-content-center align-content-center ">
+        <div class="p-4 hero-bottom-single-box rounded-3 col-3 pb-5">
+            <div class="hero-bottom-img-box p-2">
+                <i class="fa-regular h1 fa-clock"></i>
+            </div>
+            <h1>Full Day Programs</h1>
+            <p class="py-4 text-light">
+                Sedut perspiciatis unde omnis iste natus error sit voluptatem accusantium.
+            </p>
+            <a href="/">
+                Learn More
+            </a>
+        </div>
+
+        <div class="p-4 hero-bottom-single-box rounded-3 col-3 pb-5">
+            <div class="hero-bottom-img-box2 p-2">
+                <i class="fa-regular h1 fa-clock"></i>
+            </div>
+            <h1>Full Day Programs</h1>
+            <p class="py-4 text-light">
+                Sedut perspiciatis unde omnis iste natus error sit voluptatem accusantium.
+            </p>
+            <a href="/">
+                Learn More
+            </a>
+        </div>
+
+        <div class="p-4 hero-bottom-single-box rounded-3 col-3 pb-5">
+            <div class="hero-bottom-img-box3 p-2">
+                <i class="fa-regular h1 fa-clock"></i>
+            </div>
+            <h1>Full Day Programs</h1>
+            <p class="py-4 text-light">
+                Sedut perspiciatis unde omnis iste natus error sit voluptatem accusantium.
+            </p>
+            <a href="/">
+                Learn More
+            </a>
+        </div>
+    </div>
+
+ </section>
+ <!----------------------------------- hero end here-------------------------------  -->
+
+ <!----------------------------------- welcome start here-------------------------------  -->
+ <section class="d-flex container-lg py-5 m-auto align-content-center  justify-content-center">
+    <div class="w-50 p-4">
+        <img class="w-100" src="https://html.rometheme.pro/kids/images/homeslide2.jpg" alt="">
+    </div>
+
+    <div class="w-50 p-4 py-0 welcome-content">
+        <h1 class="h1 ">Welcome to Kids</h1>
+        <p class="py-2">Teritatis et quasi architecto. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent. Sed ut perspiciatis unde omnis iste natus error sitdolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod. Praesent interdum est gravida vehicula est node maecenas loareet morbi a dosis luctus novum est praesent. Magna est consectetur interdum modest dictum.</p>
+        <p class="py-2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo invent.</p>
+        <a href="/">
+            Learn More
+        </a>
+    </div>
+ </section>
+ <!----------------------------------- welcome end here-------------------------------  -->
+
+<!----------------------------------- analize start here-------------------------------  -->
+<section class="position-relative analize">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShgp1o0Vq097O1TSKtXZaKVweDVVGcNDDVRg&usqp=CAU" alt="">
+    <div class="row m-auto justify-content-center py-5">
+
+        <div class="col-2 text-center p-5">
+            <div class="roted">
+                <h1>852</h1>
+            </div>
+            <h1 class="text-light text-center py h4 -2">Students</h1>
+        </div>
+
+        <div class="col-2 text-center p-5">
+            <div class="roted2">
+                <h1>125</h1>
+            </div>
+            <h1 class="text-light text-center py h4 -2">Teachers</h1>
+        </div>
+
+        <div class="col-2 text-center p-5">
+            <div class="roted3">
+                <h1>32</h1>
+            </div>
+            <h1 class="text-light text-center py h4 -2">Class Rooms</h1>
+        </div>
+
+        <div class="col-2 text-center p-5">
+            <div class="roted4">
+                <h1>852</h1>
+            </div>
+            <h1 class="text-light text-center py h4 -2">Bus Schools</h1>
+        </div>
+
+        
+
+    </div>
+</section>
+ <!----------------------------------- analize end here-------------------------------  -->
+
+ <!----------------------------------- program start here-------------------------------  -->
+ <section class="text-center program py-5">
+    <h5>Our Programs</h5>
+    <h1>Popular Classes</h1>
+
+    <div class="row justify-content-center py-5">
+
+        <div class="program-single-box col-3">
+            <img class="w-100" src="https://html.rometheme.pro/kids/images/classes-1.jpg" alt="">
+            <div class="d-flex align-content-center justify-content-center py-2">
+                <h1 class="w-50 fw-bolder h5">Drawing Classes</h1>
+                <h1 class="w-50 h5 text-right">Drawing Classes</h1>
+            </div>
+            <p class="text-info">Open Class : 08:00 am - 10:00 am</p>
+            <p class="py-2 text-left">We provide high quality design at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores...Age 2-5 years</p>
+
+            <div class="d-flex align-content-center justify-content-center">
+                <p class="p-3 h5 w-50 text-light bg-danger">Age 2-5 years</p>
+                <p class="p-3 h5 w-50 text-light bg-info">Age 2-5 years</p>
+            </div>
+        </div>
+
+        <div class="program-single-box col-3">
+            <img class="w-100" src="https://html.rometheme.pro/kids/images/classes-1.jpg" alt="">
+            <div class="d-flex align-content-center justify-content-center py-2">
+                <h1 class="w-50 fw-bolder h5">Drawing Classes</h1>
+                <h1 class="w-50 h5 text-right">Drawing Classes</h1>
+            </div>
+            <p class="text-info">Open Class : 08:00 am - 10:00 am</p>
+            <p class="py-2 text-left">We provide high quality design at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores...Age 2-5 years</p>
+
+            <div class="d-flex align-content-center justify-content-center">
+                <p class="p-3 h5 w-50 text-light bg-danger">Age 2-5 years</p>
+                <p class="p-3 h5 w-50 text-light bg-info">Age 2-5 years</p>
+            </div>
+        </div>
+
+        <div class="program-single-box col-3">
+            <img class="w-100" src="https://html.rometheme.pro/kids/images/classes-1.jpg" alt="">
+            <div class="d-flex align-content-center justify-content-center py-2">
+                <h1 class="w-50 fw-bolder h5">Drawing Classes</h1>
+                <h1 class="w-50 h5 text-right">Drawing Classes</h1>
+            </div>
+            <p class="text-info">Open Class : 08:00 am - 10:00 am</p>
+            <p class="py-2 text-left">We provide high quality design at vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores...Age 2-5 years</p>
+
+            <div class="d-flex align-content-center justify-content-center">
+                <p class="p-3 h5 w-50 text-light bg-danger">Age 2-5 years</p>
+                <p class="p-3 h5 w-50 text-light bg-info">Age 2-5 years</p>
+            </div>
+        </div>
+
+    </div>
+    <a href="/">
+        Learn More
+    </a>
+ </section>
+ <!----------------------------------- program end here-------------------------------  -->
+
+ @endsection
---
+++ resources/views/shared/menu.blade.php
@@ -0,0 +1,70 @@
+<section>
+
+    <!-- top menu  -->
+    <div class="light-dark">
+        <div class="container-lg m-auto d-flex align-content-center ">
+            <div class="w-50 d-flex align-content-center text-light ">
+                <div class="d-flex align-content-center justify-content-center h6 pt-2 px-2">
+                    <i class="fa-solid pt-1 h6 fa-phone"></i>
+                    <p class="fw-bold mb-0 px-2"> +62 7144 3300</p>
+                </div>
+
+                <div class="d-flex align-content-center justify-content-center h6 pt-2 px-2">
+                    <i class="fa-solid pt-1 h6 fa-envelope"></i>
+                    <p class="fw-bold mb-0 px-2"> support@kids.com</p>
+                </div>
+            </div>
+
+            <div class="w-50 d-flex align-content-center justify-content-end text-light ">
+                <div class="d-flex align-content-center justify-content-center h6">
+                    <i class="fa-brands p-2 px-3 bg-dark  fa-facebook-f"></i>
+                </div>
+
+                <div class="d-flex align-content-center justify-content-center h6">
+                    <i class="fa-brands p-2 px-3 top-menu-icon-bg fa-twitter"></i>
+                </div>
+
+                <div class="d-flex align-content-center justify-content-center h6">
+                    <i class="fa-brands p-2 px-3 bg-danger fa-instagram"></i>
+                </div>
+
+                <div class="d-flex align-content-center justify-content-center h6">
+                    <i class="fa-brands p-2 px-3 bg-primary fa-linkedin-in"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- main menu  -->
+    <menu class="container-lg m-auto">
+        <div class="d-flex align-content-center justify-content-center py-4">
+            <div class="w-25">
+                <img class="img-fluid" src="https://html.rometheme.pro/kids/images/logo.png" alt="">
+            </div>
+    
+            <div class="w-75 pt-4">
+                <ul class="d-flex nav align-content-center">
+                    <li class="nav-icon">
+                        <a class="nav-link a-hover full-rounded a-btn px-4 py-2 h6" href="/">HOME</a>
+                    </li>
+                    <li class="nav-icon">
+                        <a class="nav-link a-hover full-rounded px-4 py-2 h6" href="/">ABOUT US</a>
+                    </li>
+                    <li class="nav-icon">
+                        <a class="nav-link a-hover full-rounded px-4 py-2 h6" href="/">CLASSES</a>
+                    </li>
+                    <li class="nav-icon">
+                        <a class="nav-link a-hover full-rounded px-4 py-2 h6" href="/">TEACHERS</a>
+                    </li>
+                    <li class="nav-icon">
+                        <a class="nav-link a-hover full-rounded px-4 py-2 h6" href="/">PAGES</a>
+                    </li>
+                    <li class="nav-icon">
+                        <a class="nav-link a-hover full-rounded px-4 py-2 h6" href="/">CONTACT US</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </menu>
+
+</section>
